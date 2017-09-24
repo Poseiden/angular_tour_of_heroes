5bed8313388d7bcb831e7bb57086d9d3f494f78d
HaiboZhu <hbzhu@thoughtworks.com>
2017-09-24 22:36:15 +0800

    create service, ngOnInit, provider of HeroService in AppComponent, extract hero data, Promise for return hero data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,21 @@
+import { HeroService } from './hero.service';
+import { HeroDetailComponent } from './hero-detail.component';
+import { Hero } from './hero';
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
-export class Hero{
-  id: number;
-  name: string;
-}
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
 
 @Component({
   selector: 'app-root',
   template: `
     <h1>{{title}}</h1>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name">
-      </div>
-    </div>
     <h2>My Heros</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero"> 
+      <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
+    <app-hero-detail [hero]='selectedHero' ></app-hero-detail>
     `,
     styles: [`
       .selected {
@@ -85,13 +65,27 @@ const HEROES: Hero[] = [
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-    `]
+    `],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of heros';
   selectedHero: Hero;
+  heroes = [];
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-  };
-  heroes=HEROES;
+  }
+
+  constructor(private heroService: HeroService) {
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
+
 }
