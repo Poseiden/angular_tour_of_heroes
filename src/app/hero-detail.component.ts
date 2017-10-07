49bba35035d9f14be606e883107d11d3fbeb2e73
HaiboZhu <hbzhu@thoughtworks.com>
2017-10-07 11:11:45 +0800

    add hero detail style.

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -7,7 +7,8 @@ import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html'
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
 })
 
 
